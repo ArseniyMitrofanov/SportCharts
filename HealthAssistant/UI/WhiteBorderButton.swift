80244a758b50581233c97e808946f3ee1bbc9eac
ArseniyMitrofanov <arseniy.2004@inbox.ru>
2024-03-22 14:52:20 +0300

    Добавил кастомную проектную кнопку

---
+++ HealthAssistant/UI/WhiteBorderButton.swift
@@ -0,0 +1,31 @@
+//
+//  WhiteBirderButton.swift
+//  HealthAssistant
+//
+//  Created by Арсений on 22.03.24.
+//
+
+import Foundation
+import UIKit
+
+final class WhiteBorderButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        makeCustomAppearance()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+       makeCustomAppearance()
+    }
+    
+    private func makeCustomAppearance() {
+        self.backgroundColor = .clear
+        self.layer.borderColor = UIColor.white.cgColor
+        self.layer.borderWidth = 2
+        self.layer.cornerRadius = 15
+        self.clipsToBounds = true
+        self.layer.masksToBounds = true
+        self.setTitleColor(.white, for: .normal)
+    }
+}
