ec3478b11eba620092f58b7474ef9347d5a0c1f6
ArseniyMitrofanov <arseniy.2004@inbox.ru>
2024-04-05 14:59:27 +0300

    Сделал красивый таббар

--- HealthAssistant/Tabbar/TabbarController.swift
+++ HealthAssistant/Tabbar/TabbarController.swift
@@ -10,60 +10,65 @@ import UIKit
 
 final class TabbarController: UITabBarController {
     
-    private var customTabBarView = UIView(frame: .zero)
+    private var whiteBackgroudView = UIView(frame: .zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hidesBottomBarWhenPushed = true
-        fillTabBar()
-        addCustomTabBarView()
+        self.fillTabBar()
+        self.setupBackgroudView()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        self.setupCustomTabBarFrame()
+        self.setupCustomTabBar()
     }
     
 }
 
 private extension TabbarController {
     
-    func setupCustomTabBarFrame() {
-        let height = self.view.safeAreaInsets.bottom + 60
+    func setupCustomTabBar() {
+        let height = self.view.safeAreaInsets.bottom + 65
         var tabFrame = self.tabBar.frame
         tabFrame.size.height = height
         tabFrame.origin.y = self.view.frame.size.height - height
         self.tabBar.frame = tabFrame
         self.tabBar.setNeedsLayout()
         self.tabBar.layoutIfNeeded()
-        self.tabBar.tintColor = .red
-        customTabBarView.frame = tabBar.frame
+        self.tabBar.tintColor = UIColor.init(named: "Backgroud")
+        self.tabBar.unselectedItemTintColor = UIColor(named: "UnselectedWhite")
+        self.whiteBackgroudView.frame = tabBar.frame
     }
     
-    func addCustomTabBarView() {
-        self.customTabBarView.frame = tabBar.frame
-        self.customTabBarView.backgroundColor = .white
-        self.customTabBarView.layer.masksToBounds = false
-        self.customTabBarView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
-        self.customTabBarView.layer.cornerRadius = 20
-        self.view.addSubview(customTabBarView)
+    func setupBackgroudView() {
+        self.whiteBackgroudView.backgroundColor = .white
+        self.whiteBackgroudView.layer.masksToBounds = false
+        self.whiteBackgroudView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        self.whiteBackgroudView.layer.cornerRadius = 20
+        self.view.addSubview(self.whiteBackgroudView)
         self.view.bringSubviewToFront(self.tabBar)
                
     }
     
-    func fillTabBar(){
+    func fillTabBar() {
         let weightViewController = WeightModuleBuilder.setupModule()
+        let adviseViewController = WeightModuleBuilder.setupModule()
         let settingsViewController = SettingsModuleBuilder.setupModule()
         
-        weightViewController.tabBarItem = UITabBarItem(title: "Weight",
-                                                       image: UIImage(systemName: "chart.line.downtrend.xyaxis"),
+        weightViewController.tabBarItem = UITabBarItem(title: "",
+                                                       image: UIImage(systemName: "chart.line.downtrend.xyaxis.circle.fill"),
                                                        tag: 0)
-        settingsViewController.tabBarItem = UITabBarItem(title: "Settings",
-                                                         image: UIImage(systemName: "gearshape"),
-                                                         tag: 1)
-     
+        //потом его напишу - пока так чисто глянуть иконки
+        adviseViewController.tabBarItem = UITabBarItem(title: "",
+                                                       image: UIImage(systemName: "book.fill"),
+                                                       tag: 1)
         
-        self.viewControllers = [weightViewController, settingsViewController]
+        settingsViewController.tabBarItem = UITabBarItem(title: "",
+                                                         image: UIImage(systemName: "gearshape.fill"),
+                                                         tag: 2)
+     
+        self.viewControllers = [weightViewController, adviseViewController, settingsViewController]
         self.selectedIndex = 0
     }
 }
