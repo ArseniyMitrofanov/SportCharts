f22c901dfacefa9816e6afd5f67ecabf66b0aa16
ArseniyMitrofanov <arseniy.2004@inbox.ru>
2024-04-11 20:32:17 +0300

    Добавил запрос на бэк на создание акка

--- HealthAssistant/CreateAccount/CreateAccountPresenter.swift
+++ HealthAssistant/CreateAccount/CreateAccountPresenter.swift
@@ -23,4 +23,48 @@ final class CreateAccountPresenter: ICreateAccountPresenter {
         self.viewController.selfDismiss()
     }
 
+    func createButtonTouchUpInside() {
+        self.viewController.endEditing()
+        
+        if let email = viewController.emailText(),
+           let password = viewController.passwordText(),
+           let repeatPassword = viewController.repeatPasswordText(),
+           password == repeatPassword {
+            let request = AuthorizationRequest(email: email, password: password)
+            self.sendCreateAccountRequest(request: request)
+        }else {
+            print("smth")
+        }
+    }
+}
+
+private extension CreateAccountPresenter {
+    func sendCreateAccountRequest(request: AuthorizationRequest) {
+        let json = request.encode()
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        if let url = URL(string:  "https://healthassistant-production.up.railway.app/api/v1.0/auth/register") {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "POST"
+            urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            urlRequest.httpBody = jsonData
+
+            URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+                guard let data = data, error == nil else {
+                    print(error?.localizedDescription ?? "No data")
+                    return
+                }
+                let decoder = JSONDecoder()
+                let tokens = try? decoder.decode(Tokens.self, from: data)
+                let httpResponse = response as! HTTPURLResponse
+                print("SIGN IN STATUS : \(httpResponse.statusCode)")
+                if (httpResponse.statusCode >= 200) && (httpResponse.statusCode < 300) && tokens != nil{
+                    print( tokens!.accessToken)
+                }else {
+                    print("no")
+                }
+            }
+            .resume()
+        }
+    }
 }
--- HealthAssistant/CreateAccount/CreateAccountRequest.swift
+++ HealthAssistant/CreateAccount/CreateAccountRequest.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct AuthorizationRequest {
+    var email: String
+    var password: String
+    
+    public func encode() -> [String: Any] {
+        return ["username": self.email,
+                "password": self.password,
+                "firstname": "firstname",
+                "lastname": "lastname"
+        ]
+    }
+}
+
+struct Tokens: Codable {
+    var accessToken: String
+    var refreshToken: String
+}
