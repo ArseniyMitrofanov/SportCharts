e73fb6aa605768317b7967a63de374f13f1831fc
ArseniyMitrofanov <arseniy.2004@inbox.ru>
2024-04-19 13:18:24 +0300

    добавил ui на addWeight

--- HealthAssistant/AddWheight/AddWeightPresenter.swift
+++ HealthAssistant/AddWheight/AddWeightPresenter.swift
@@ -8,14 +8,17 @@
 import Foundation
 
 protocol IAddWeightPreesenter {
+    func viewDidLoad()
     func addButtonTapped()
     func backButtonTouhUpInside()
+    func pickerViewDidSelectRow(at row: Int)
 }
 
 final class AddWeightPresenter: IAddWeightPreesenter {
     
     private unowned var viewController: IAddWeightViewController
-    
+    private var selectedWeightValue = 0
+    private var getCurrentDateString = ""
     init(viewController: IAddWeightViewController) {
         self.viewController = viewController
     }
@@ -26,6 +29,24 @@ final class AddWeightPresenter: IAddWeightPreesenter {
     
     func backButtonTouhUpInside() {
         self.viewController.selfDismiss()
+    }
+    
+    func pickerViewDidSelectRow(at row: Int) {
+        self.selectedWeightValue = row
+    }
+    
+    func viewDidLoad() {
+        let dateText = "Сегодня: " + self.getCurrentDateTime()
+        self.viewController.setDateLabelText(dateText)
+    }
+}
+
+private extension IAddWeightPreesenter {
+    func getCurrentDateTime() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy-HH:mm"
+        self.getCurrentDateString = formatter.string(from: Date())
+        return self.getCurrentDateString
     }
 }
 
--- HealthAssistant/AddWheight/AddWeightViewController.swift
+++ HealthAssistant/AddWheight/AddWeightViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 
 protocol IAddWeightViewController: AnyObject {
     func selfDismiss()
+    func setDateLabelText(_ text: String)
 }
 
 final class AddWeightViewController: UIViewController {
     var presenter: IAddWeightPreesenter?
     
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    @IBOutlet weak var addbutton: UIButton!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
     @IBOutlet weak var backButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.presenter?.viewDidLoad()
         self.setupUI()
     }
     @IBAction func backButtonTouhUpInside(_ sender: Any) {
@@ -26,13 +34,45 @@ final class AddWeightViewController: UIViewController {
 }
 
 extension AddWeightViewController: IAddWeightViewController {
+    func setDateLabelText(_ text: String) {
+        self.dateLabel.text = text
+    }
+    
     func selfDismiss() {
         self.dismiss(animated: true)
     }
 }
 
+extension AddWeightViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 701
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(row)
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.presenter?.pickerViewDidSelectRow(at: row)
+    }
+    
+}
+
 private extension AddWeightViewController {
     func setupUI() {
         self.backButton.setTitle("", for: .normal)
+        self.setupAddButton()
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
+    }
+    
+    func setupAddButton() {
+        self.addbutton.layer.cornerRadius = 15
+        self.addbutton.clipsToBounds = true
+        self.addbutton.layer.masksToBounds = true
     }
 }
